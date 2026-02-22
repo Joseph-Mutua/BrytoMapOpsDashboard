9613a6a170ace3e29d70957bc4debcac0d6e79c0
Joseph Mutua <mutuaj793@gmail.com>
2026-02-22 16:49:07 +0300

    feat: add street view coverage planner

--- src/pages/StreetViewOpsPage.tsx
+++
@@ -1,5 +0,0 @@
-import { PlaceholderPage } from '@/pages/PlaceholderPage';
-
-export function StreetViewOpsPage() {
-  return <PlaceholderPage title="Street View Coverage Planner + Publishing Status" />;
-}
